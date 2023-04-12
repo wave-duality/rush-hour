1f825c1b50895acd5c22d6af48045a53bc1ae260
WaveDuality <zhanghantai@gmail.com>
2023-04-11 21:16:00 -0700

    Update rush hour bot.py
    -Changed DFS to BFS after analyzing game mechanisms, which guarantees that the program returns shortest solution
    -Implemented hash value function for every board state to more quickly check whether a state has been visited (set())
    -Implemented priority queue and heuristic function to prioritize more "likely" solutions through A*-like algorithm
    -Fixed bugs

--- rush hour bot.py
+++ rush hour bot.py
@@ -1,4 +1,8 @@
 import copy
+from queue import PriorityQueue
+import time
+import random
+from math import *
 #coordinates start at 1 at the top, 1 at the left
 class Car:
     def __init__(self, x, y, length, orient):
@@ -24,29 +28,20 @@ class Car:
 
 
 class State:
-    MAX_CAR = 10
-
-    def __init__(self, n, board, cars, currmoves):
-        self.board_size = n
+    def __init__(self, board, cars, currmoves):
         self.board = board
         self.cars = cars
-        self.currmoves = currmoves   
-
-    def hash_value(self):
-        val = 0
-        for i in range(0, self.board_size):
-            for j in range(0, self.board_size):
-                if i > 0 or j > 0:
-                    val *= self.MAX_CAR
-                val += self.board[i][j]
-        return val
+        self.currmoves = currmoves
 
     def __eq__(self, other):
-        for i in range(0, self.board_size):
-            for j in range(0, self.board_size):
+        for i in range(0, len(self.board)):
+            for j in range(0, len(self.board[0])):
                 if self.board[i][j] != other.board[i][j]:
                     return False
         return True
+
+    def __lt__(self, other):
+        return random.randint(0, 2)
 
 
 def toggle(n):
@@ -149,28 +144,113 @@ def applymove(move, oldstate):
 
 
 def check(state, visited):
-    return state.hash_value() in visited
+    for i in visited:
+        if i == state:
+            return True
+    return False
 
-visited = set() #stores instances of "State"
+def hashvalue(state):
+    #hashes each board state into a number
+    res = 0
+    base = len(state.cars)+1
+    power = 0
+    for i in range(0, len(state.board)):
+        for j in range(0, len(state.board)):
+            res += (base**power)*state.board[i][j]
+            power += 1
+    return res
 
-def dfs(state):
-    #if too long, terminate
-    #explore neighbors, mark as visited
-    #if return, mark as unvisited
-    if state.cars[0].x == target[0] and state.cars[0].y == target[1]:
-        print("Found a Solution! (" + str(len(state.currmoves)) + " moves)")
-        for i in state.currmoves: print(i)
+def blocking(state):
+    global n
+    board = state.board
+    #return the number of cars currently in the way from the red car to the exit
+    blocks = {0}
+    cars = state.cars
+    for x in range(cars[0].x+cars[0].length-1, n):
+        if board[cars[0].y-1][x] not in blocks:
+            blocks.add(board[cars[0].y-1][x])
+    return (len(blocks)-1)
+
+def heuristic(state):
+    #A* considers the number of moves from the start and the expected # of moves to finish
+    return (len(state.currmoves))
+
+def printboard(state):
+    dimension = len(state.board)
+    res = "- "
+    for j in range(3*dimension):
+        res = res + "- "
+    res = res + "\n"
+    for i in range(len(state.board)):
+        line1 = "|     "
+        for j in range(dimension):
+            line1 = line1+ "|     "
+        line1 = line1 + "\n"
+        res = res + line1
+        line2 = "|"
+        for j in range(dimension):
+            if state.board[i][j] == 0:
+                line2 = line2 + "  " + str(" ") + "  |"
+            else:
+                line2 = line2 + "  " + str(chr(state.board[i][j]+64)) + "  |"
+        line2 = line2 + "\n"
+        res = res + line2
+        line3 = "|"
+        for j in range(dimension):
+            line3 = line3 + "_ _ _|"
+        line3 = line3 + "\n"
+        res = res + line3
+    print(res)
+
+def directions(state, move):
+    car = state.cars[move[0]-1]
+    if car.orient == 1:
+        if move[1] > 0:
+            return " " + str(move[1]) + " squares to the right."
+        else:
+            return " " + str((-1)*move[1]) + " squares to the left."
     else:
-        v = state.hash_value()
-        visited.add(v)
-        moves = validmoves(state)
-        for i in moves:
-            new = applymove(i, state)
-            if not check(new, visited) and len(new.currmoves) < 10:
-                dfs(new)
-        visited.remove(v)
+        if move[1] > 0:
+            return " " + str(move[1]) + " squares downward."
+        else:
+            return " " + str((-1)*move[1]) + " squares upward."
+    
+def printsol(state):
+    #start from the beginning, print the board one step at a time
+    global initial
+    print("Initial Board:")
+    printboard(initial)
+    step = 1
+    for i in state.currmoves:
+        initial = applymove(i, initial)
+        print("Step #" + str(step) + ": " + "Move car \"" + chr(i[0]+64) + "\"" + directions(state, i))
+        printboard(initial)
+        step += 1
         
+visited = set() #stores instances of "State"
+pq = PriorityQueue()
+solved = False
+st = time.time()
 
+def bfs():
+    global solved, pq, visited
+    index = 0
+    while not solved:
+        curr = pq.get()[1]
+        if curr.cars[0].x == target[0] and curr.cars[0].y == target[1]:
+            solved = True
+            print("Found a solution of length " + str(len(curr.currmoves)) + "!")
+            for i in curr.currmoves: print(i)
+            printsol(curr)
+            print("Runtime: " + str(time.time()-st) + " sec")
+        for i in validmoves(curr):
+            new = applymove(i, curr)
+            if hashvalue(new) not in visited:
+                visited.add(hashvalue(new))
+                pq.put((heuristic(new), new))
+        index += 1
+        
+            
 target = [5, 3] #once our model left-right length 2 red car arrives here, the game is won
 n, startcars = takeInput()
 
@@ -186,10 +266,12 @@ for i in range(1, len(startcars)+1):
         startboard[k[1]-1][k[0]-1] = i
 
 
-initial = State(n, startboard, startcars, [])
+initial = State(startboard, startcars, [])
+pq.put((0, initial))
 
 
-dfs(initial)
+bfs()
+
 
 
 
